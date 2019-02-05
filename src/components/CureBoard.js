59463da2106aa7a37b7269e53aa4dcca8d64385b
dboicourt <dboicourt@gmail.com>
2019-02-05 14:30:48 -0800

    feat(cureboard)
    cureboard has all colors

--- src/components/CureBoard.js
+++ src/components/CureBoard.js
@@ -29,25 +29,49 @@ class CureBoard extends Component {
     if (colorCards.length >= 4) {
       this.props.firebase.database().update(updates);
     } else {
-      alert("Not enough Red Cards");
+      alert(`Not enough ${color} Cards`);
     }
   }
   render() {
     return (
       <Card>
-        <Grid>
+        <Grid container alignItems="center" justify="center">
           <Grid item>
             <ButtonBase
               onClick={() => {
                 this.handleCure("red");
               }}
             >
-              <img src="/assets/redCure.png" />
+              <img height="42" width="42" src="/assets/redCure.png" />
             </ButtonBase>
           </Grid>
-          <Grid item />
-          <Grid item />
-          <Grid item />
+          <Grid item>
+            <ButtonBase
+              onClick={() => {
+                this.handleCure("blue");
+              }}
+            >
+              <img height="42" width="42" src="/assets/blueCure.png" />
+            </ButtonBase>
+          </Grid>
+          <Grid item>
+            <ButtonBase
+              onClick={() => {
+                this.handleCure("black");
+              }}
+            >
+              <img height="42" width="42" src="/assets/blackCure.png" />
+            </ButtonBase>
+          </Grid>
+          <Grid item>
+            <ButtonBase
+              onClick={() => {
+                this.handleCure("yellow");
+              }}
+            >
+              <img height="42" width="42" src="/assets/yellowCure.png" />
+            </ButtonBase>
+          </Grid>
         </Grid>
       </Card>
     );
