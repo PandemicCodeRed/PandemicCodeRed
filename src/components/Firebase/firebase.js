5c2952e52b6496cc1953f8428e1f831adbb3d289
dboicourt <dboicourt@gmail.com>
2019-02-08 11:54:02 -0800

    feat(firebase)
    Points to correct DB

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,47 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+//  const prodConfig = {
+//   apiKey: "AIzaSyC5MueShoA7ir8IbGtBrY9xgikEUcO2-rg",
+//   authDomain: "pandemic-cr.firebaseapp.com",
+//   databaseURL: "https://pandemic-cr.firebaseio.com",
+//   projectId: "pandemic-cr",
+//   storageBucket: "pandemic-cr.appspot.com",
+//   messagingSenderId: "101783012584"
+// };
+
+//  const devConfig = {
+//   apiKey: "AIzaSyBCo4W6yWle1FH8jZhe8yhIENUuuGCi5nQ",
+//   authDomain: "dev-pandemic.firebaseapp.com",
+//   databaseURL: "https://dev-pandemic.firebaseio.com",
+//   projectId: "dev-pandemic",
+//   storageBucket: "dev-pandemic.appspot.com",
+//   messagingSenderId: "881104490800"
+// };
+
+//  const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+const config = {
+  apiKey: "AIzaSyC5MueShoA7ir8IbGtBrY9xgikEUcO2-rg",
+  authDomain: "pandemic-cr.firebaseapp.com",
+  databaseURL: "https://pandemic-cr.firebaseio.com",
+  projectId: "pandemic-cr",
+  storageBucket: "pandemic-cr.appspot.com",
+  messagingSenderId: "101783012584"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+    this.db = app.database();
+  }
+
+  // *** User API ***
+
+  database = () => this.db.ref();
+  playerOne = () => this.db.ref("playerOne");
+}
+
+export default Firebase;
